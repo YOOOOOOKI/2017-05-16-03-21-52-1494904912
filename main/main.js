3e00a75d2348234f7891934242372892e584303b
YOOOOOOKI <zyqzyqzyq106@gmail.com>
2017-06-03 20:12:00 +0800

    第一次提交感觉可能不对因为太简单

--- main/main.js
+++ main/main.js
@@ -1,3 +1,11 @@
 module.exports = function main(email, suffixes) {
   // Write your code here
+  if (email.indexOf(suffixes[0]!=-1)) 
+  {
+  	return true;
+  }
+  else
+  {
+  	return false;
+  }
 };
